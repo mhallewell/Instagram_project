19bb0f0e2496ad9299d55e3c4f521736f6bb05ea
MARY HALLEWELL <mhallewell@gmail.com>
2015-11-06 09:20:42 -0700

    Links now call .handlebars pages

--- routes/marketingRoutes.js
+++ routes/marketingRoutes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res) {
+router.get('/index', function(req, res) {
 
   res.render('index', {
     title: 'This is a title!',
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,4 +9,28 @@ router.get('/dashboard', function(req, res) {
   })
 })
 
+router.get('/profile', function(req, res) {
+  res.render('profile', {
+    layout: 'auth_base',
+    title: 'User Profile!',
+    welcome: 'Welcome to your user profile'
+  })
+})
+
+router.get('/search', function(req, res) {
+  res.render('search', {
+    layout: 'auth_base',
+    title: 'Search Page',
+    welcome: 'Welcome to your searches'
+  })
+})
+
+router.get('/savedsearches', function(req, res) {
+  res.render('savedsearches', {
+    layout: 'auth_base',
+    title: 'Saved Searches',
+    welcome: 'Welcome to your saved searches'
+  })
+})
+
 module.exports = router
