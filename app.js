caa3a6a3eda3906f7434e6ca1db016af505d155f
MARY HALLEWELL <mhallewell@gmail.com>
2015-11-13 09:08:51 -0700

    After user logs in Dashboard is now rendered

--- app.js
+++ app.js
@@ -16,15 +16,15 @@ app.set('view engine', 'handlebars');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', marketingRoutes)
-app.use('/user', userRoutes)
-
 app.use(session({
   cookieName: 'session',
   secret: 'thisismysecret',
   resave: false,
   saveUninitialized: true
 }))
+
+app.use('/', marketingRoutes)
+app.use('/user', userRoutes)
 
 //app.use(bodyParser.urlencoded({extended: false })) //parses data into a usable object
 app.get('/', function(req, res){
@@ -70,28 +70,9 @@ app.get('/auth/finalize', function(req, res){
 
 
 
-app.get('/user/dashboard', function (req, res, next) {
-  var options = {
-    url: 'https://api.instagram.com/v1/users/self/feed?access_token='+ req.session.access_token
-  }
-  request.get(options, function(error, response, body){
-    if(error) {return next(error)}
-
-    try{
-        var feed = JSON.parse(body)
-    }
-    catch(err){
-      return res.redirect('/')
-    }
-
-  if(feed.meta.code > 200){
-    return next(feed.meta.error_message)
-  }
-    res.render('feed', {
-      feed: feed.data
-    })
-  })
-})
+// app.get('/user/dashboard', function (req, res, next) {
+//
+// })
 // app.use(function(err, req, res, next){
 //   res.status(err.status || 500);
 //   res.render('error', {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,34 @@
 var express = require('express');
+var request = require('request')
 var router = express.Router();
 
-router.get('/dashboard', function(req, res) {
-  res.render('dashboard', {
-    layout: 'auth_base',
-    title: 'User Dashboard!',
-    welcome: 'Welcome to your dashboard!'
+router.get('/dashboard', function(req, res, next) {
+  // res.render('dashboard', {
+  //   layout: 'auth_base',
+  //   title: 'User Dashboard!',
+  //   welcome: 'Welcome to your dashboard!'
+  // })
+  console.log('here')
+  var options = {
+    url: 'https://api.instagram.com/v1/users/self/feed?access_token='+ req.session.access_token
+  }
+  request.get(options, function(error, response, body){
+    if(error) {return next(error)}
+
+    try{
+        var feed = JSON.parse(body)
+    }
+    catch(err){
+      return res.redirect('/')
+    }
+
+    if(feed.meta.code > 200){
+      return next(feed.meta.error_message)
+    }
+    console.log(feed)
+    res.render('dashboard', {
+      feed: feed.data
+    })
   })
 })
 
